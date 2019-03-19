40c462843c6b02b64ad97df5d58803c7a2e708ab
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-19 11:09:43 +0100

    Initialize Spring Boot project with dependencies and .gitignore file

---
+++ src/main/java/com/gymchampion/GymChampion/GymChampionApplication.java
@@ -0,0 +1,13 @@
+package com.gymchampion.GymChampion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymChampionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymChampionApplication.class, args);
+	}
+
+}
