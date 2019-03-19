1b787767f88a9b406e1cc7721c8ce38aaba64e57
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-19 18:35:13 +0100

    Refactor Gender class

--- src/main/java/com/gymchampion/GymChampion/model/Gender.java
+++ src/main/java/com/gymchampion/GymChampion/model/Gender.java
@@ -4,28 +4,30 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
 @Table(name="gender")
 public class Gender {
-
 
     @Id
     @Column(name = "gender_id")
     @GeneratedValue
     private int genderId;
 
-    @Column(nullable = false, length = 15)
+    @Column(name="sex", nullable = false, length = 15)
     private String sex;
 
     @OneToMany(mappedBy = "gender")
-    private List<User> user = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+
+    public Gender() {}
 
     public Gender(String sex) {
         this.sex = sex;
     }
 
     public int getGenderId() {
-        return genderId;
+        return this.genderId;
     }
 
     public void setGenderId(int genderId) {
@@ -33,10 +35,18 @@ public class Gender {
     }
 
     public String getSex() {
-        return sex;
+        return this.sex;
     }
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public List<User> getUsers() {
+        return this.users;
+    }
+
+    public void setUser(List<User> user) {
+        this.users = users;
     }
 }
