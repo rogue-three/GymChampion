de5f86e9af4b138039b8e0881ed6a10755778c56
Michal Wisniewski <mihuwis@gmail.com>
2019-03-20 13:49:34 +0100

    Add Body part interface and init

--- src/main/java/com/gymchampion/GymChampion/service/DBConstantsInitializer.java
+++ src/main/java/com/gymchampion/GymChampion/service/DBConstantsInitializer.java
@@ -1,7 +1,9 @@
 package com.gymchampion.GymChampion.service;
 
+import com.gymchampion.GymChampion.model.BodyPart;
 import com.gymchampion.GymChampion.model.Equipment;
 import com.gymchampion.GymChampion.model.Gender;
+import com.gymchampion.GymChampion.repository.BodyPartRepository;
 import com.gymchampion.GymChampion.repository.EquipmentRepository;
 import com.gymchampion.GymChampion.repository.GenderRepository;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,8 @@ import org.springframework.stereotype.Component;
 public class DBConstantsInitializer {
 
     public DBConstantsInitializer(GenderRepository genderRepository,
-                                  EquipmentRepository equipmentRepository) {
+                                  EquipmentRepository equipmentRepository,
+                                  BodyPartRepository bodyPartRepository) {
 
         genderRepository.save(new Gender("male"));
         genderRepository.save(new Gender("female"));
@@ -18,6 +21,13 @@ public class DBConstantsInitializer {
         equipmentRepository.save(new Equipment("dumbbell"));
         equipmentRepository.save(new Equipment("machine"));
         equipmentRepository.save(new Equipment("no equipment"));
+        bodyPartRepository.save(new BodyPart("shoulders"));
+        bodyPartRepository.save(new BodyPart("back"));
+        bodyPartRepository.save(new BodyPart("chest"));
+        bodyPartRepository.save(new BodyPart("quads"));
+        bodyPartRepository.save(new BodyPart("hams"));
+        bodyPartRepository.save(new BodyPart("arms"));
+        bodyPartRepository.save(new BodyPart("calves"));
     }
 
 
