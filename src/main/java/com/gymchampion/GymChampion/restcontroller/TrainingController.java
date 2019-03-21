f18787842353d708004c7e8aa68dfc412c24707a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 05:36:22 +0100

    Inject dependencies in TrainingController and TrainingService classes

--- src/main/java/com/gymchampion/GymChampion/restcontroller/TrainingController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/TrainingController.java
@@ -1,7 +1,18 @@
 package com.gymchampion.GymChampion.restcontroller;
 
+import com.gymchampion.GymChampion.service.TrainingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/training")
 public class TrainingController {
+
+    private TrainingService trainingService;
+
+    @Autowired
+    public TrainingController(TrainingService trainingService) {
+        this.trainingService = trainingService;
+    }
 }
--- src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
+++ src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
@@ -1,7 +1,16 @@
 package com.gymchampion.GymChampion.service;
 
+import com.gymchampion.GymChampion.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public TrainingService(UserRepository userRepository) {
+        this.userRepository= userRepository;
+    }
 }
