85fe4f6cc2cfcd7d5855dbe84a5abc09c67af122
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 20:58:54 +0100

    Implement SessionService and SessionRepository

--- src/main/java/com/gymchampion/GymChampion/repository/SessionRepository.java
+++ src/main/java/com/gymchampion/GymChampion/repository/SessionRepository.java
@@ -3,5 +3,11 @@ package com.gymchampion.GymChampion.repository;
 import com.gymchampion.GymChampion.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SessionRepository extends JpaRepository<Session, Integer> {
+
+    Session findBySessionKey(String key);
+
+    List<Session> findAllByActive(boolean isActive);
 }
