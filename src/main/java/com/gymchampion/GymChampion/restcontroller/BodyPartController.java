1e3386269385e48b714c7c114e1264fe88c93efd
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 19:18:48 +0100

    Implement BodyPartController

--- src/main/java/com/gymchampion/GymChampion/restcontroller/BodyPartController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/BodyPartController.java
@@ -1,7 +1,35 @@
 package com.gymchampion.GymChampion.restcontroller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.gymchampion.GymChampion.model.BodyPart;
+import com.gymchampion.GymChampion.service.BodyPartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/body_part")
 public class BodyPartController {
+
+    private BodyPartService bodyPartService;
+
+    @Autowired
+    public BodyPartController(BodyPartService bodyPartService) {
+        this.bodyPartService = bodyPartService;
+    }
+
+    @GetMapping
+    public List<BodyPart> getAllBodyParts() {
+        return this.bodyPartService.getAllBodyParts();
+    }
+
+    @GetMapping("/{id}")
+    public List<BodyPart> getBodyPartById(@PathVariable("id") int id) {
+        return this.bodyPartService.getBodyPartById(id);
+    }
+
+    @PostMapping
+    public BodyPart addBodyPart(@RequestBody BodyPart bodyPart) {
+        return this.bodyPartService.addBodyPart(bodyPart);
+    }
 }
