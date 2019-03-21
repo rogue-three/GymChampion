b5a73800000f90b139e292a5a66ec87e8e91272e
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 21:36:50 +0100

    Change logger dependency, filepath in log4j.properties and implement simple logger usage in GymChampionApplication class

--- src/main/java/com/gymchampion/GymChampion/GymChampionApplication.java
+++ src/main/java/com/gymchampion/GymChampion/GymChampionApplication.java
@@ -1,12 +1,16 @@
 package com.gymchampion.GymChampion;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymChampionApplication {
 
+	public static Logger logger = Logger.getLogger(GymChampionApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Gym Champion REST application starting...");
 		SpringApplication.run(GymChampionApplication.class, args);
 	}
 
