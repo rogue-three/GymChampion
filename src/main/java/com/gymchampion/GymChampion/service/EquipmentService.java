54081748ee00a1ffc6b8edec55225a0b8955705b
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 19:32:03 +0100

    Implement EquipmentService

--- src/main/java/com/gymchampion/GymChampion/service/EquipmentService.java
+++ src/main/java/com/gymchampion/GymChampion/service/EquipmentService.java
@@ -1,7 +1,38 @@
 package com.gymchampion.GymChampion.service;
 
+import com.gymchampion.GymChampion.model.Equipment;
+import com.gymchampion.GymChampion.repository.EquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EquipmentService {
+
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    public EquipmentService(EquipmentRepository equipmentRepository) {
+        this.equipmentRepository = equipmentRepository;
+    }
+
+    public List<Equipment> getAllEquipments() {
+        return this.equipmentRepository.findAll();
+    }
+
+    public Equipment getEquipmentById(int id) {
+        Optional<Equipment> optionalBodypart = this.equipmentRepository.findById(id);
+
+        if(optionalBodypart.isPresent()) {
+            return optionalBodypart.get();
+        }
+        return new Equipment();
+    }
+
+    public Equipment addEquipment(Equipment equipment) {
+        return this.equipmentRepository.save(equipment);
+    }
+
 }
