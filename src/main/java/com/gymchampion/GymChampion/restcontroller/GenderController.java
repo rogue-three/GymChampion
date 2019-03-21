499b9d7ec9f5e3ffa5296e3ef58a62d3ed9965f1
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 20:09:55 +0100

    Implement GenderController and GenderService

--- src/main/java/com/gymchampion/GymChampion/restcontroller/GenderController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/GenderController.java
@@ -1,7 +1,27 @@
 package com.gymchampion.GymChampion.restcontroller;
 
+import com.gymchampion.GymChampion.model.Gender;
+import com.gymchampion.GymChampion.service.GenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/gender")
 public class GenderController {
+
+    private GenderService genderService;
+
+    @Autowired
+    public GenderController(GenderService genderService) {
+        this.genderService = genderService;
+    }
+
+    @GetMapping
+    public List<Gender> getAllGenders() {
+        return this.genderService.getAllGenders();
+    }
 }
--- src/main/java/com/gymchampion/GymChampion/service/GenderService.java
+++ src/main/java/com/gymchampion/GymChampion/service/GenderService.java
@@ -1,7 +1,23 @@
 package com.gymchampion.GymChampion.service;
 
+import com.gymchampion.GymChampion.model.Gender;
+import com.gymchampion.GymChampion.repository.GenderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenderService {
+
+    private GenderRepository genderRepository;
+
+    @Autowired
+    public GenderService(GenderRepository genderRepository) {
+        this.genderRepository = genderRepository;
+    }
+
+    public List<Gender> getAllGenders() {
+        return this.genderRepository.findAll();
+    }
 }
