006e2b0eba0b747f70894d808c1c81fcff597eef
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 05:49:10 +0100

    Implement add training feature

--- src/main/java/com/gymchampion/GymChampion/restcontroller/TrainingController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/TrainingController.java
@@ -1,9 +1,11 @@
 package com.gymchampion.GymChampion.restcontroller;
 
+import com.gymchampion.GymChampion.model.Training;
 import com.gymchampion.GymChampion.service.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/training")
@@ -14,5 +16,10 @@ public class TrainingController {
     @Autowired
     public TrainingController(TrainingService trainingService) {
         this.trainingService = trainingService;
+    }
+
+    @PostMapping
+    public void addTraining(@RequestBody Training training) {
+        this.trainingService.addTraining(training);
     }
 }
--- src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
+++ src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
@@ -1,16 +1,21 @@
 package com.gymchampion.GymChampion.service;
 
-import com.gymchampion.GymChampion.repository.UserRepository;
+import com.gymchampion.GymChampion.model.Training;
+import com.gymchampion.GymChampion.repository.TrainingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
 
-    private UserRepository userRepository;
+    private TrainingRepository trainingRepository;
 
     @Autowired
-    public TrainingService(UserRepository userRepository) {
-        this.userRepository= userRepository;
+    public TrainingService(TrainingRepository trainingRepository) {
+        this.trainingRepository = trainingRepository;
+    }
+
+    public void addTraining(Training training) {
+        this.trainingRepository.save(training);
     }
 }
