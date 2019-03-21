e05801d4c49ab929c1ae0c91a9ff8c53574de4a9
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 19:29:24 +0100

    Implement EquipmentController

--- src/main/java/com/gymchampion/GymChampion/restcontroller/EquipmentController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/EquipmentController.java
@@ -1,7 +1,35 @@
 package com.gymchampion.GymChampion.restcontroller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.gymchampion.GymChampion.model.Equipment;
+import com.gymchampion.GymChampion.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/equipment")
 public class EquipmentController {
+
+    private EquipmentService equipmentService;
+
+    @Autowired
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
+    }
+
+    @GetMapping
+    public List<Equipment> getAllEquipments() {
+        return this.equipmentService.getAllEquipments();
+    }
+
+    @GetMapping("/{id}")
+    public Equipment getEquipmentById(@PathVariable("id") int id) {
+        return this.equipmentService.getEquipmentById(id);
+    }
+
+    @PostMapping
+    public Equipment addEquipment(@RequestBody Equipment equipment) {
+        return this.equipmentService.addEquipment(equipment);
+    }
 }
