7c54f9f35fd83e0b3b1307d234b5b7aea34f9033
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-21 20:15:56 +0100

    Initialize LoginDataController and LoginDataService

--- src/main/java/com/gymchampion/GymChampion/restcontroller/SetSchemeController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/SetSchemeController.java
@@ -1,6 +1,5 @@
 package com.gymchampion.GymChampion.restcontroller;
 
-import com.gymchampion.GymChampion.model.Exercise;
 import com.gymchampion.GymChampion.service.SetSchemeService;
 import com.gymchampion.GymChampion.model.SetScheme;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,13 +27,11 @@ public class SetSchemeController {
     @PostMapping
     public SetScheme saveScheme(@RequestBody SetScheme scheme) {
         return setSchemeService.addSchemeToRepository(scheme);
-
     }
 
     @GetMapping("/{training_id}")
     public List<SetScheme> getSchemeByTrainingId(@PathVariable("training_id") int trainingId) {
         return  setSchemeService.getSchemeByTrainingId(trainingId);
-
     }
 
     @GetMapping("/max/{login}/{exerciseId}")
