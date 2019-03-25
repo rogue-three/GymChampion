2a283abc857d5b85ee01e2c227ff6d629587f9a9
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-25 17:37:39 +0100

    Refactor LoginData repository

--- src/main/java/com/gymchampion/GymChampion/repository/LoginDataRepository.java
+++ src/main/java/com/gymchampion/GymChampion/repository/LoginDataRepository.java
@@ -2,11 +2,9 @@ package com.gymchampion.GymChampion.repository;
 
 import com.gymchampion.GymChampion.model.LoginData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface LoginDataRepository extends JpaRepository<LoginData, Integer> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM login_data WHERE login_data.user_login = :login")
-    LoginData findLoginDataByUserLogin(@Param("login") String login);
+    LoginData findByUser_Login(@Param("login") String login);
 }
