a95071472e387406ec8b2756c30ae34c6c5a25f4
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-25 17:49:32 +0100

    Refactor TrainingService

--- src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
+++ src/main/java/com/gymchampion/GymChampion/service/TrainingService.java
@@ -42,20 +42,20 @@ public class TrainingService {
         return this.trainingRepository.findTrainingsByArchivized(false);
     }
 
-    public List<Training> getAllArchivizedTrainings() {
-        return this.trainingRepository.findTrainingsByArchivized(true);
+    public List<Training> getAllArchivedTrainings() {
+        return this.trainingRepository.findTrainingsByArchived(true);
     }
 
     public List<Training> getTrainingsByUserLogin(String login) {
         return this.trainingRepository.findTrainingsByUserLogin(login);
     }
 
-    public Training archivizeTraining(int id) {
+    public Training archiveTraining(int id) {
         Optional<Training> optionalTraining = this.trainingRepository.findById(id);
         Training training;
         if (optionalTraining.isPresent()) {
             training = optionalTraining.get();
-            training.setArchivized(true);
+            training.setArchived(true);
             return this.trainingRepository.save(training);
         }
         return new Training();
