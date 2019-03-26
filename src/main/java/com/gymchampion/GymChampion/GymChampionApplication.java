f08d8c373b6a26f4a54b2cbe0e83c5f8906e3b39
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-03-26 21:32:59 +0100

    Put filter on all api routes.

--- src/main/java/com/gymchampion/GymChampion/GymChampionApplication.java
+++ src/main/java/com/gymchampion/GymChampion/GymChampionApplication.java
@@ -1,5 +1,6 @@
 package com.gymchampion.GymChampion;
 
+import com.gymchampion.GymChampion.security.GymApiFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -14,4 +15,13 @@ public class GymChampionApplication {
 		SpringApplication.run(GymChampionApplication.class, args);
 	}
 
+
+	@Bean
+	public FilterRegistrationBean setFilter() {
+
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new GymApiFilter());
+		filterRegistrationBean.setUrlPatterns(Collections.singleton("/api/*"));
+		return filterRegistrationBean;
+	}
 }
