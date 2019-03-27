c4bf1f2d61b6d94b0ecb4fc7d2aa2e5c170b7bbc
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-03-27 22:05:50 +0100

    Implement auto set new user role to "USER".

--- src/main/java/com/gymchampion/GymChampion/security/SignInUserController.java
+++ src/main/java/com/gymchampion/GymChampion/security/SignInUserController.java
@@ -1,8 +1,10 @@
 package com.gymchampion.GymChampion.security;
 
 import com.gymchampion.GymChampion.model.LoginData;
+import com.gymchampion.GymChampion.model.Role;
 import com.gymchampion.GymChampion.model.User;
 import com.gymchampion.GymChampion.service.LoginDataService;
+import com.gymchampion.GymChampion.service.RoleService;
 import com.gymchampion.GymChampion.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,17 +18,21 @@ public class SignInUserController {
 
     private LoginDataService loginDataService;
     private UserService userService;
+    private RoleService roleService;
 
     @Autowired
-    public SignInUserController(LoginDataService service, UserService userService) {
+    public SignInUserController(LoginDataService service, UserService userService, RoleService roleService) {
         this.loginDataService = service;
         this.userService = userService;
+        this.roleService = roleService;
     }
 
 
     @PostMapping
     public User addUser(@RequestBody LoginData data) {
         loginDataService.addLoginData(data);
+        Role user = roleService.getRoleByRoleName("USER");
+        data.setUserRole(user);
         return this.userService.addUser(data.getUser());
     }
 
