ade9794756a2c8f00fd0ee65c0736b82232872ad
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-29 13:39:33 +0100

    Migrate BodyPart-, Equipment- and ExerciseSchemeController functionality into ExerciseController

---
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/DiagnosticsController.java
@@ -0,0 +1,9 @@
+package com.gymchampion.GymChampion.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/diagnostics")
+public class DiagnosticsController {
+}
--- src/main/java/com/gymchampion/GymChampion/restcontroller/ExerciseController.java
+++ src/main/java/com/gymchampion/GymChampion/restcontroller/ExerciseController.java
@@ -1,5 +1,11 @@
 package com.gymchampion.GymChampion.restcontroller;
 
+import com.gymchampion.GymChampion.model.BodyPart;
+import com.gymchampion.GymChampion.model.Equipment;
+import com.gymchampion.GymChampion.model.ExerciseScheme;
+import com.gymchampion.GymChampion.service.BodyPartService;
+import com.gymchampion.GymChampion.service.EquipmentService;
+import com.gymchampion.GymChampion.service.ExerciseSchemeService;
 import com.gymchampion.GymChampion.service.ExerciseService;
 import com.gymchampion.GymChampion.model.Exercise;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +14,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/exercise")
+@RequestMapping("/v1/exercise")
 public class ExerciseController {
 
     private ExerciseService exerciseService;
+    private BodyPartService bodyPartService;
+    private EquipmentService equipmentService;
+    private ExerciseSchemeService exerciseSchemeService;
 
     @Autowired
-    public ExerciseController(ExerciseService service) {
+    public ExerciseController(ExerciseService service,
+                              BodyPartService bodyPartService,
+                              EquipmentService equipmentService,
+                              ExerciseSchemeService exerciseSchemeService) {
         this.exerciseService = service;
+        this.bodyPartService = bodyPartService;
+        this.equipmentService = equipmentService;
+        this.exerciseSchemeService = exerciseSchemeService;
     }
+
+    /*
+     *   Exercise handling
+     */
 
     @GetMapping
     public List<Exercise> getAllExercises() {
@@ -63,5 +82,61 @@ public class ExerciseController {
         return this.exerciseService.setExerciseEquipment(exercise);
     }
 
+    /*
+     *   BodyPart handling
+     */
 
+
+    @GetMapping("/body_part")
+    public List<BodyPart> getAllBodyParts() {
+        return this.bodyPartService.getAllBodyParts();
+    }
+
+    @GetMapping("/body_part/{id}")
+    public BodyPart getBodyPartById(@PathVariable("id") int id) {
+        return this.bodyPartService.getBodyPartById(id);
+    }
+
+    @PostMapping("/body_part/admin")
+    public BodyPart addBodyPart(@RequestBody BodyPart bodyPart) {
+        return this.bodyPartService.addBodyPart(bodyPart);
+    }
+
+    /*
+     *   Equipment handling
+     */
+
+    @GetMapping("/equipment")
+    public List<Equipment> getAllEquipments() {
+        return this.equipmentService.getAllEquipments();
+    }
+
+    @GetMapping("/equipment/{id}")
+    public Equipment getEquipmentById(@PathVariable("id") int id) {
+        return this.equipmentService.getEquipmentById(id);
+    }
+
+    @PostMapping("/equipment/admin")
+    public Equipment addEquipment(@RequestBody Equipment equipment) {
+        return this.equipmentService.addEquipment(equipment);
+    }
+
+    /*
+     *   ExerciseScheme handling
+     */
+
+    @GetMapping("/exercise_scheme")
+    public List<ExerciseScheme> getAllExerciseSchemes() {
+        return this.exerciseSchemeService.getAllExerciseSchemes();
+    }
+
+    @GetMapping("/exercise_scheme/{id}")
+    public ExerciseScheme getExerciseSchemeById(@PathVariable("id") int id) {
+        return this.exerciseSchemeService.getExerciseSchemeById(id);
+    }
+
+    @PostMapping("/exercise_scheme/admin")
+    public ExerciseScheme addExerciseScheme(@RequestBody ExerciseScheme exerciseScheme) {
+        return this.exerciseSchemeService.addExerciseScheme(exerciseScheme);
+    }
 }
